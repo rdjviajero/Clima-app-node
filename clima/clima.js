6398f8da013503f411b4849f11688984d39c9c11
rdjviajero <34464889+rdjviajero@users.noreply.github.com>
2020-07-10 18:57:33 -0500

    Update clima.js

--- clima/clima.js
+++ clima/clima.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 // 
 const getClima = async (lat,lon)=>{
-  const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=a9dbc6cc3324fb6fc5adf6af03e49a7a`); 
+  const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid={APIKEY}`); 
   return resp.data.main.temp;
 }
 
